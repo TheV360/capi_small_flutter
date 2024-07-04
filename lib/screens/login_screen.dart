38212c9389ebd3944f2df3cc8490fff58b293647
V360 <v360code@gmail.com>
2024-07-04 02:40:49 -0400

    better loading state for login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -24,6 +24,7 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _doLogin() async {
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
     setState(() => _inProgress = true);
 
     final username = usernameController.text;
@@ -63,57 +64,68 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Log in'),
+        bottom: PreferredSize(
+          preferredSize: const Size(double.infinity, 4),
+          child: Visibility(
+            visible: _inProgress,
+            child: const LinearProgressIndicator(),
+          ),
+        ),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Spacer(),
-                TextFormField(
-                  controller: usernameController,
-                  maxLines: 1,
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.catching_pokemon),
-                    labelText: 'Username',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Enter a username.';
-                    }
-                    return null;
-                  },
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints.loose(const Size.fromWidth(360)),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(flex: 3),
+                    TextFormField(
+                      controller: usernameController,
+                      enabled: !_inProgress,
+                      maxLines: 1,
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.catching_pokemon),
+                        labelText: 'Username',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Enter a username.';
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      enabled: !_inProgress,
+                      maxLines: 1,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        icon: Icon(Icons.key),
+                        labelText: 'Password',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Enter a password.';
+                        } else if (value.length < 4) {
+                          return 'Enter a longer password.';
+                        }
+                        return null;
+                      },
+                    ),
+                    const Spacer(),
+                    ElevatedButton(
+                      onPressed: _inProgress ? null : _doLogin,
+                      child: const Text('Log in'),
+                    ),
+                    const Spacer(flex: 3)
+                  ],
                 ),
-                TextFormField(
-                  controller: passwordController,
-                  enabled: !_inProgress,
-                  maxLines: 1,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    icon: Icon(Icons.key),
-                    labelText: 'Password',
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Enter a password.';
-                    } else if (value.length < 4) {
-                      return 'Enter a longer password.';
-                    }
-                    return null;
-                  },
-                ),
-                Spacer(),
-                ElevatedButton(
-                  onPressed: _inProgress ? null : _doLogin,
-                  child: _inProgress
-                      ? const CircularProgressIndicator()
-                      : const Text('Log in'),
-                ),
-                Spacer()
-              ],
+              ),
             ),
           ),
         ),
